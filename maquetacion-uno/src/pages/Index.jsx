0f75a0d13144659414f78eff7a8aacc723503100
Elgrinch01 <sbohorquez@cesde.net>
2026-04-24 21:35:49 -0500

    Coreccion del index

--- maquetacion-uno/src/pages/Index.jsx
+++ maquetacion-uno/src/pages/Index.jsx
@@ -7,28 +7,27 @@ export default function Index() {
   const navigate = useNavigate();
 
   return (
-    <>
+    <div className="home-page">
       <NavBarHeader />
 
-      <section className="hero">
+      <main className="hero" aria-label="Presentacion principal">
         <div className="hero-content">
           <h1>
             El arte de la <br /> paciencia
           </h1>
 
           <p>
-            Descubre la serenidad a través del cuidado y diseño de árboles Bonsái.
-            <br />
-            Un espacio para reconectar con la naturaleza.
+            Descubre la serenidad a través del cuidado y diseño de árboles Bonsái. Un espacio
+            para reconectar con la naturaleza.
           </p>
 
           <button className="btn-primary" onClick={() => navigate("/reservas")}>
             Reserva un taller
           </button>
         </div>
-      </section>
+      </main>
 
       <Footer />
-    </>
+    </div>
   );
 }
